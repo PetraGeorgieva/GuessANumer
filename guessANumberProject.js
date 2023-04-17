653d41de5033eac84dff04b57e0d8bfdece7d242
PetraGeorgieva <Petra.kirilova.georgieva@gmail.com>
2023-04-17 14:09:15 +0200

    .

--- guessANumberProject.js
+++ guessANumberProject.js
@@ -10,8 +10,8 @@ function guessANumber() {
   let recursiveAsyncReadLine = function () {
     readline.question('Guess the number (0-100): ', number => {
       guess = Number(number);
-
       if (guess <= 100 && guess >= 0) {
+
         if (guess == computerGuess) {
           console.log('You guess it!');
           return readline.close()
